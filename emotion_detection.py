e6c82bcec14529899711126a221b82bec0e9e3b2
Venkata Vamsi Madhav Pitta <vmcse09@gmail.com>
2025-02-23 14:20:18 +0530

    TASK 3 Finished

---
+++ emotion_detection.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+
+def emotion_detector(text_to_analyze):
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = {"raw_document": {"text": text_to_analyze}}
+
+    response = requests.post(url, headers=headers, json=input_json)
+    response_dict = json.loads(response.text)
+
+    emotions = response_dict["emotion_predictions"][0]["emotion"]
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    return {
+        "anger": emotions["anger"],
+        "disgust": emotions["disgust"],
+        "fear": emotions["fear"],
+        "joy": emotions["joy"],
+        "sadness": emotions["sadness"],
+        "dominant_emotion": dominant_emotion,
+    }
